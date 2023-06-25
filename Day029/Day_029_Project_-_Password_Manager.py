e40f48ec46033e8de2fd8c8983b98f500e90032b
Ngozi <124025329+fancifulcrow@users.noreply.github.com>
2023-06-25 21:00:34 +0100

    Update Day_029_Project_-_Password_Manager.py

--- Day029/Day_029_Project_-_Password_Manager.py
+++ Day029/Day_029_Project_-_Password_Manager.py
@@ -1,9 +1,22 @@
 import tkinter
+from tkinter import END
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_field.get()
+    email = username_field.get()
+    password = password_field.get()
+
+    with open("passwords_list.txt", "a") as f:
+        f.write(f"Website: {website}, Email: {email}, Password: {password}\n")
+
+    website_field.delete(0, END)
+    password_field.delete(0, END)
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = tkinter.Tk()
@@ -22,23 +35,25 @@ website_label.grid(row=1, column=0)
 
 website_field = tkinter.Entry(width=35)
 website_field.grid(row=1, column=1, columnspan=2)
+website_field.focus()
 
 username_label = tkinter.Label(text="Email/Username:")
 username_label.grid(row=2, column=0)
 
 username_field = tkinter.Entry(width=35)
 username_field.grid(row=2, column=1, columnspan=2)
+username_field.insert(0, "johndoe@gmail.com")
 
 password_label = tkinter.Label(text="Password:")
 password_label.grid(row=3, column=0)
 
-password_field = tkinter.Entry(width=20)
-password_field.grid(row=3, column=1, columnspan=2)
+password_field = tkinter.Entry(width=35)
+password_field.grid(row=3, column=1)
 
 generate_password_button = tkinter.Button(text="Generate Password")
-generate_password_button.grid(row=3, column=2)
+generate_password_button.grid(row=4, column=1)
 
-add_button = tkinter.Button(text="Add", width=30)
-add_button.grid(row=4, column=1, columnspan=2)
+add_button = tkinter.Button(text="Add", width=30, command=save)
+add_button.grid(row=5, column=1, columnspan=2)
 
 window.mainloop()
