a0e33a0729cd7c363393665d008d520bb91943d7
Ngozi <124025329+fancifulcrow@users.noreply.github.com>
2023-08-21 22:11:03 +0100

    Day 064 (complete)

--- Day064/main.py
+++ Day064/main.py
@@ -6,27 +6,131 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
 
-On Windows type:
-python -m pip install -r requirements.txt
+from dotenv import load_dotenv
+import os
 
-On MacOS type:
-pip3 install -r requirements.txt
+load_dotenv()
 
-This will install the packages from requirements.txt for this project.
-'''
+MOVIE_DB_API_KEY = os.getenv("MOVIE_DB_API_KEY")
+MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
+MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
+MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
+db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies-list.db"
+db.init_app(app)
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.String(250), nullable=True)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+# new_movie = Movie(
+#     title="Phone Booth",
+#     year=2002,
+#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#     rating=7.3,
+#     ranking=10,
+#     review="My favourite character was the caller.",
+#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+# )
+# with app.app_context():
+#     db.session.add(new_movie)
+#     db.session.commit()
+
+
+class EditForm(FlaskForm):
+    new_rating = StringField("Your Rating out of 10 e.g. 7.5")
+    new_review = StringField("Your Review")
+    submit = SubmitField("Edit Rating")
+
+
+class AddForm(FlaskForm):
+    movie_title = StringField("Movie Title", validators=[DataRequired()])
+    submit = SubmitField("Add Movie")
+
+
+with app.app_context():
+    db.create_all()
+
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    result = db.session.execute(db.select(Movie.rating))
+    all_movies = result.scalars().all()
+
+    for i in range(len(all_movies)):
+        all_movies[i].ranking = len(all_movies) - i
+    db.session.commit()
+    
+    return render_template("index.html", movies=all_movies)
+
+
+@app.route("/edit/<int:movie_id>", methods=["GET", "POST"])
+def edit(movie_id):
+    movie_to_edit = db.get_or_404(Movie, movie_id)
+    edit_form = EditForm()
+
+    if edit_form.validate_on_submit():
+        movie_to_edit.rating = edit_form.new_rating.data
+        movie_to_edit.review = edit_form.new_review.data
+        db.session.commit()
+        return redirect(url_for('home'))
+
+    return render_template("edit.html", movie=movie_to_edit, edit_form=edit_form)
+
+
+@app.route("/delete/<int:movie_id>", methods=["GET", "POST"])
+def delete(movie_id):
+    movie_to_delete = db.get_or_404(Movie, movie_id)
+    db.session.delete(movie_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+@app.route("/add")
+def add():
+    add_form = AddForm()
+    if add_form.validate_on_submit():
+        movie_title = add_form.title.data
+
+        response = requests.get(MOVIE_DB_SEARCH_URL, params={"api_key": MOVIE_DB_API_KEY, "query": movie_title})
+        data = response.json()["results"]
+        return render_template("select.html", options=data)
+
+    return render_template("add.html", add_form=add_form)
+
+
+@app.route("/find")
+def find_movie():
+    movie_api_id = request.args.get("id")
+    if movie_api_id:
+        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
+        response = requests.get(movie_api_url, params={"api_key": MOVIE_DB_API_KEY, "language": "en-US"})
+        data = response.json()
+        new_movie = Movie(
+            title=data["title"],
+            year=data["release_date"].split("-")[0],
+            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+            description=data["overview"]
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        
+        # Redirect to /edit route
+        return redirect(url_for("edit", id=new_movie.id))
 
 
 if __name__ == '__main__':
