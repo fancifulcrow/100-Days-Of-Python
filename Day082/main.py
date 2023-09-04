69f42847774421955b7519f93123c9257b525420
Ngozi <124025329+fancifulcrow@users.noreply.github.com>
2023-09-04 22:57:24 +0100

    Day 082

---
+++ Day082/main.py
@@ -0,0 +1,17 @@
+from text_to_morse_code import text_to_morse_code
+import winsound
+
+text = input("Input a text you would like to convert to morse code?\n").lower()
+
+output = ""
+
+for char in text:
+    try:
+        morse = text_to_morse_code[char]
+    except KeyError:
+        output = f"{char} cannot be converted to morse code"
+        break
+    else:
+        output += morse
+
+print(output)
